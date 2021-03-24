6bdf7c19a2e8f30a290e016a07db796ff3f4ba1e
Dong Yi Liang <liang8717@uwlax.edu>
2021-03-25 00:58:51 +0800

    3.25

---
+++ my-app/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from "axios";
+import './index.css';
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.baseUrl = 'https://api.mugglepay.com/api/usdt';
+        this.state = {};
+    }
+
+    render() {
+        const item = '1 USDT is roughly';
+        let price = this.state.price;
+        return (
+            <div className="content">
+                <h1>Hello World!</h1>
+                <h1>{item}</h1>
+                <h1>{price}CNY</h1>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        axios.get(this.baseUrl).then(res=>{
+            this.setState({price:res.data.price});//get data from api
+        })
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
